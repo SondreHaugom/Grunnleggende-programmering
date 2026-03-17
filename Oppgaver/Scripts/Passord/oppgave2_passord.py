0f32102dcaec237fb1c50ba19dd3a4e524a33ca5
SondreHaugom <sondrehaugom@gmail.com>
2026-03-17 12:31:47 +0100

    Refaktoerte "er_gyldig_passord" funksjonen med re modulen for enklere skjekk av ugyldige verdier for passord

--- Oppgaver/Scripts/Passord/oppgave2_passord.py
+++ Oppgaver/Scripts/Passord/oppgave2_passord.py
@@ -1,17 +1,13 @@
+import re
 def er_gyldig_passord(passord: str) -> bool:
     if len(passord) < 10:
+        return False    
+    if not re.search(r'[A-Z]', passord):
         return False
-    if not any(char.isupper() for char in passord):
+    if not re.search(r'[a-z]', passord):
         return False
-    
-    if not any(char.islower() for char in passord):
+    if not re.search(r'[0-9]', passord):
         return False
-    
-    if not any(char.isdigit() for char in passord):
-        return False
-    
     if ' ' in passord:
         return False
-        
-    
     return True

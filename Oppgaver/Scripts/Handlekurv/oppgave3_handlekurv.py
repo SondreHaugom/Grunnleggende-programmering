83768b098a759b7faad7a2389f5ac6b0bf80531a
SondreHaugom <sondrehaugom@gmail.com>
2026-03-18 13:22:22 +0100

    Simulert en enklet handlekurv

--- Oppgaver/Scripts/Handlekurv/oppgave3_handlekurv.py
+++ Oppgaver/Scripts/Handlekurv/oppgave3_handlekurv.py
@@ -1,13 +1,20 @@
 def handlekurv():
     handlekurv =  []
 
-    while True:
-        produkt = input("Skriv inn produktet du vil legge inn i handlekurven: ")
-        if produkt.lower() ==  "q":
-            break
-        handlekurv.append(produkt)
-        print(f"Produktet '{handlekurv}' er lagt til i handlekurven.")
+    varer = { "melk": 22.9, "brød": 15.5, "ost": 45.0, "egg": 30.0, "smør": 25.0}
 
+    while True:
+        print("Velkommen til handlekurven! Skriv 'q' for å avslutte.")
+        for vare, pris in varer.items():
+            print(f"{vare}: {pris} kr")
+        valg = input("Skriv inn navnet på varen du vil legge i handlekurven: ")
+        if valg.lower() == 'q':
+            break
+        elif valg in varer:
+            handlekurv.append(valg)
+            print(f"{valg} har blitt lagt i handlekurven. {varer[valg]} kr")
+        else:
+            print("Ugyldig valg. Vennligst prøv igjen.")
 
 if __name__ == "__main__":
     handlekurv()

dffa37626e3f2ae7c31e890630ff8f7f3435cbab
SondreHaugom <sondrehaugom@gmail.com>
2026-03-20 09:59:51 +0100

    Refaktorert litt og fått til: leg til vare og vise vare

--- Oppgaver/Scripts/Handlekurv/oppgave3_handlekurv.py
+++ Oppgaver/Scripts/Handlekurv/oppgave3_handlekurv.py
@@ -1,15 +1,119 @@
-# funksjon for handlekurv som oppretter en tom handlekurv og en ordbok med varer og priser, og returnerer begge
-def handlekurv():
-    handlekurv =  [] # tom liste som fylles seg opp med varer og priser
+def main():
+    handlekurv = {"Vare": [], "Pris": []}  # Flytt denne utenfor while-løkka
+    
+    while True:
+        menu()
+        valg = input("Velg et alternativ (1-4): ")
 
+        if valg == '1':
+            handlekurv = legg_til_varer(handlekurv)  # Send handlekurv som parameter
+        
+        elif valg == '2':
+            vis_handlekurv(handlekurv)
+        
+        elif valg == '3':
+            print("Slett vare - ikke implementert enda")
+            
+        elif valg == '4':
+            print("Gå til kassen - ikke implementert enda")
+            break
+            
+        else:
+            print("Ugyldig valg. Velg 1-4.")
+
+
+
+
+def menu():
+    print("Velkommen til handlekurven!")
+    print("1. Legg til vare")
+    print("2. Vis handlekurv")
+    print("3. Slett vare")
+    print("4. Gå til kassen")
+
+
+
+
+
+# funksjon for å legge til varer i handlekurven
+def legg_til_varer(handlekurv_liste):    
     varer = { "melk": 22.9, "brød": 15.5, "ost": 45.0, "egg": 30.0, "smør": 25.0}
-    if not varer:
-        print("Ingen varer tilgjengelig.")
-        return
-    return handlekurv, varer
+
+    while True:
+        print("\nTilgjengelige varer:")
+        for vare, pris in varer.items():
+            print(f"{vare}: {pris} kr")
+        print("\nSkriv inn navnet på varen og prisen for å legge den i handlekurven (trykk 'q' for å avslutte): ")
+        vare = input("Vare: ")
+        if vare.lower() == 'q':
+            print("Takk for handleturen!")
+            break
+        try:
+            pris = float(input("Pris: "))
+            handlekurv_liste["Vare"].append(vare)
+            handlekurv_liste["Pris"].append(pris)
+        except ValueError:
+            print("Ugyldig pris. Vennligst skriv inn et gyldig tall.")
+    return handlekurv_liste 
 
 
 
+def vis_handlekurv(handlekurv_liste):
+    print("\nDin handlekurv:")
+    if not handlekurv_liste["Vare"]:
+        print("Handlekurven er tom.")
+    else:
+        for vare, pris in zip(handlekurv_liste["Vare"], handlekurv_liste["Pris"]):
+            print(f"{vare} - {pris} kr")
+
+    return handlekurv_liste
+
+
+
+
+
+"""
+
+def slett_vare(handlekurv):
+    print("Vil du fjerne en vare fra handlekurven? (ja/nei): ")
+    fjern_vare = input()
+    if fjern_vare.lower() == 'ja':
+        vare_fjern = input("Skriv inn navnet på varen du vil fjerne: ")
+        handlekurv = [item for item in handlekurv if item['Vare'].lower() != vare_fjern.lower()]
+        print(f"{vare_fjern} fjernet fra handlekurven.")
+    return handlekurv
+
+
+"""
+        
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 if __name__ == "__main__":
     handlekurv, varer = handlekurv()
     print("Velkommen til handlekurven!")
@@ -62,5 +166,4 @@ if __name__ == "__main__":
 
         except ValueError:
             print("Ugyldig pris. Vennligst skriv inn et gyldig tall.")
-
- 
+"""
